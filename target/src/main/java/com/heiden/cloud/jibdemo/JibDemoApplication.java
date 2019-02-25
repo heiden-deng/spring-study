49113f728e0a0d99db83e187f201c04010261e30
heiden.deng <heiden_deng@163.com>
2019-02-25 10:26:30 +0800

    add jib example code

---
+++ target/src/main/java/com/heiden/cloud/jibdemo/JibDemoApplication.java
@@ -0,0 +1,12 @@
+package com.heiden.cloud.jibdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibDemoApplication.class, args);
+	}
+}
